4f3d5b7d28011dc0850be2440c92f3cc686b3c99
409879654@qq.com <anini456123>
2019-01-10 16:11:05 +0800

    init

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ps -aux | grep 8888 | awk '{print $2}' | xargs kill -9
 gunicorn miaosha.wsgi:application -b 0.0.0.0:8888 -w 8 -t 300 --reload
+
+# gunicorn miaosha.wsgi:application -b 0.0.0.0:8888 -w 8 -k gthread --threads 40 --max-requests 4096 --max-requests-jitter 512

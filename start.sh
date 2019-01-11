80d7bbc74407de451f27033febfacfca81c79ab8
409879654@qq.com <anini456123>
2019-01-11 11:41:20 +0800

    init

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ps -aux | grep 8888 | awk '{print $2}' | xargs kill -9
 #gunicorn miaosha.wsgi:application -b 0.0.0.0:8888 -w 8 -t 300 --reload
-gunicorn miaosha.wsgi:application -b 0.0.0.0:8888 -w 12 -k gthread --threads 50 --max-requests 5120 --max-requests-jitter 512 --reload
+gunicorn miaosha.wsgi:application -b 0.0.0.0:8888 -w 8 -k gthread --threads 50 --max-requests 4048 --max-requests-jitter 512 --reload
